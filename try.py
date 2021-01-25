a17c7deddf8d19ca99eb694fac552dfdc3b7d608
Alma Frankenstein <a.q.frankenstein@gmail.com>
2021-01-25 15:27:28 -0800

    only loop over valid tiles

--- try.py
+++ try.py
@@ -7,13 +7,13 @@ from PIL import Image
 # TODO Automate the tile counts
 # TODO Include Metadata (artist, title, etc)
 
-root_url = "https://d32dm0rphc51dk.cloudfront.net/dAMtqpwtIUgN0zlJpjYrmA/dztiles/12/{}_{}.jpg"   # Dali
-# root_url = "https://d32dm0rphc51dk.cloudfront.net/z6cZrfbgQXCnoZPztYQTsQ/dztiles/11/{}_{}.jpg"  # Mucha
+# root_url = "https://d32dm0rphc51dk.cloudfront.net/dAMtqpwtIUgN0zlJpjYrmA/dztiles/12/{}_{}.jpg"   # Dali
+root_url = "https://d32dm0rphc51dk.cloudfront.net/z6cZrfbgQXCnoZPztYQTsQ/dztiles/11/{}_{}.jpg"  # Mucha
 
 # Mucha 3x3
 # Dali 5x8 (final: Fetching image 4_7.jpg)
-TILE_WIDTH_RANGE = 9
-TILE_HEIGHT_RANGE = 9
+TILE_WIDTH_RANGE = 10
+TILE_HEIGHT_RANGE = 10
 TILE_SIZE = 512
 
 # TODO It should be safe to assume the first tile is enough to determine the maximum TILE_SIZE.
@@ -25,33 +25,45 @@ new_image = Image.new('RGB', (TILE_SIZE * TILE_WIDTH_RANGE, TILE_SIZE * TILE_HEI
 #  actual_width = (tile_count_width - 1) * TILE_SIZE + SMALLEST_WIDTH
 actual_width = 0
 actual_height = 0
-height_counter = 0
 width_counter = 0
+height_counter = 0
+
+# count width:
+for i in range(TILE_WIDTH_RANGE):
+  r = requests.get(root_url.format(i, 0))
+  if r.ok:
+    width_counter += 1
+    
+# print(f"{width_counter} tiles across")
+
+# count height:
+for j in range(TILE_HEIGHT_RANGE):
+  r = requests.get(root_url.format(0, j))
+  if r.ok:
+    height_counter += 1
+    
+# print(f"{height_counter} tiles high")
 
 # TODO Parallelize the tile fetch
 
-for i in range(TILE_WIDTH_RANGE):
-  for j in range(TILE_HEIGHT_RANGE): 
-    try:
-      r = requests.get(root_url.format(i, j))
-      im = Image.open(BytesIO(r.content))
-      width, height = im.size
-      actual_height += height
-      actual_width += width
-      new_image.paste(im, (TILE_SIZE * i, TILE_SIZE * j))
-      print(f"Fetching image {i}_{j}.jpg")
-      if r.ok and j == 0:
-        height_counter += 1
-      if r.ok and i == 0:
-        width_counter += 1
-    except OSError:
-      pass
+for i in range(width_counter):
+  for j in range(height_counter): 
+    r = requests.get(root_url.format(i, j))
+    if not r.ok:
+      break
+    im = Image.open(BytesIO(r.content))
+    width, height = im.size
+    actual_height += height
+    actual_width += width
+    new_image.paste(im, (TILE_SIZE * i, TILE_SIZE * j))
+    print(f"Fetching image {i}_{j}.jpg")
 
-actual_width /= width_counter
-actual_height /=  height_counter
+actual_width /= height_counter
+actual_height /=  width_counter
 
 print(f"Image size computed at: {actual_width}x{actual_height} (NOT {new_image.size})")
 cropped_image = new_image.crop((0, 0, actual_width, actual_height))
+cropped_image.save("img.png")
 cropped_image.show()
 
-# TODO Turn the whole thing into a flask app and host it on GH?
+# # TODO Turn the whole thing into a flask app and host it on GH?
