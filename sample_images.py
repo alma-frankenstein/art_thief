b997db9d542173b275a0a9b42cea69955a2e3e0d
Alma Frankenstein <a.q.frankenstein@gmail.com>
2021-01-30 09:33:16 -0800

    loop through and show list of sample images

--- sample_images.py
+++ sample_images.py
@@ -4,6 +4,11 @@ from main import *
 sample_images = ["https://www.artsy.net/artwork/man-ray-cadeau-1926", "https://www.artsy.net/artwork/alphonse-mucha-job-30",
                  "https://www.artsy.net/artwork/salvador-dali-madonne"]
 
-du_nord = "https://www.artsy.net/artwork/joseph-cornell-hotel-du-nord-little-durer"
-root_url = image_url(du_nord)
-fabulous_picture(root_url)
+# du_nord = "https://www.artsy.net/artwork/joseph-cornell-hotel-du-nord-little-durer"
+# root_url = image_url(du_nord)
+# fabulous_picture(root_url)
+
+for url in sample_images:
+    root_url = image_url(url)
+    fabulous_picture(root_url)
+    
