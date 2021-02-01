339a303e401f36779d63841ad3b0a02c29f30cf5
Alma Frankenstein <a.q.frankenstein@gmail.com>
2021-02-01 15:57:36 -0800

    use CLI for multiple urls at once

--- show_me.py
+++ show_me.py
@@ -1,38 +1,18 @@
 import PIL
 import argparse
-import os
-import sys
+# import os
+# import sys
 
 from format_url import image_url
 from main import fabulous_picture
 
-parser = argparse.ArgumentParser(description="show artsy pictures")
+parser = argparse.ArgumentParser(description="Enter '--nargs' followed by the urls, in quotes. Saves images in file tree.")
+# example: 
+# python3 show_me.py --nargs "https://www.artsy.net/artwork/salvador-dali-madonne" "https://www.artsy.net/artwork/vivian-maier-0117585-jeweler-through-window"
 
-parser.add_argument('urls',
-                    # type=list,
-                    help='a list of artsy urls as strings. Example: "https://www.artsy.net/artist-series/felix-gonzalez-torres-candy" ')
-
+parser.add_argument('--nargs', nargs='+')
 args = parser.parse_args()
 
-input_urls = args.urls
-
-# for url in input_urls:
-#     root_url = image_url(url)
-#     fabulous_picture(root_url)
-
-root_url = image_url(input_urls)
-fabulous_picture(root_url)
-
-
-
-# sample_images = ["https://www.artsy.net/artwork/vivian-maier-0117585-jeweler-through-window", "https://www.artsy.net/artwork/joseph-cornell-hotel-du-nord-little-durer",
-#                  "https://www.artsy.net/artwork/salvador-dali-madonne", "https://www.artsy.net/artwork/david-wojnarowicz-arthur-rimbaud-in-new-york-diner-2"]
-
-
-# # sample_images = ["https://www.artsy.net/artwork/salvador-dali-madonne"]
-# # sample_images = ["https://www.artsy.net/artwork/nick-cave-until-8"]
-
-# for url in sample_images:
-#     root_url = image_url(url)
-#     fabulous_picture(root_url)
-    
+for url in args.nargs:
+    root_url = image_url(url)
+    fabulous_picture(root_url)
