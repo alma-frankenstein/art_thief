72d30e27d8bfdd49d8b6bc2599678fce3dc25111
Alma Frankenstein <a.q.frankenstein@gmail.com>
2021-02-09 17:38:00 -0800

    start from dztiles/13 for every image and count down. Some try/except/pass code smell in save_pic.py

--- save_pic.py
+++ save_pic.py
@@ -5,12 +5,6 @@ from main import get_tiles_and_save
 from urllib.parse import urljoin
 
 
-# def amend_dz_url(root_url):
-#     stem = root_url[:-12]
-#     new_dz_num = "9/{}_{}.jpg"
-#     new_dz_url = urljoin(stem, new_dz_num)
-#     return new_dz_url
-
 # def amend_dz_url(root_url:str, dz_num:str="9") -> str:
 #     stem = root_url.rsplit("/", 2)
 #     stem[-2] = dz_num # TODO Why???????
@@ -30,16 +24,24 @@ def save_pic(artsy_url):
         try:
             get_tiles_and_save(root_url, jpeg_label)
         except SystemError as e:
-            dz_num_counter = 11
+            dz_num_counter = 12
             logging.error(f"attempting to fetch: {artsy_url}")
             logging.error(f"artwork name: {jpeg_label}")
             try:
                 while dz_num_counter > 8:
                     amended_dz_url = amend_dz_url(root_url, dz_num_counter)
                     logging.info(f"dztiles number was too large. trying {dz_num_counter} as {amended_dz_url}")
-                    if get_tiles_and_save(amended_dz_url, jpeg_label):
-                        logging.info(f"Success at {dz_num_counter} with url {amended_dz_url}")
+                    # if get_tiles_and_save(amended_dz_url, jpeg_label):
+                    #     logging.info(f"Success at {dz_num_counter} with url {amended_dz_url}")
+                    #     break
+                    # else:
+                    #     dz_num_counter -= 1
+                    #     continue
+                    try:
+                        get_tiles_and_save(amended_dz_url, jpeg_label)
                         break
+                    except SystemError:
+                        pass
                     dz_num_counter -= 1
             except:
                 raise e
@@ -55,8 +57,9 @@ def save_pic(artsy_url):
 # save_pic("https://www.artsy.net/artwork/georges-mazilu-portrait-de-femme")
 # save_pic("https://www.artsy.net/artwork/stanley-whitney-untitled-449")
 # save_pic("https://www.artsy.net/artwork/andy-warhol-camouflage-set-of-5-skateboard-decks")  # 10
-save_pic("https://www.artsy.net/artwork/greg-gong-untitled-7")   # 10
+# save_pic("https://www.artsy.net/artwork/greg-gong-untitled-7")   # 10
 # save_pic("https://www.artsy.net/artwork/vik-muniz-handmade-noise") # 10
 
 
 # print(amend_dz_url("https://d32dm0rphc51dk.cloudfront.net/naV_9uqzYITVYviI1V2iHA/dztiles/10/{}_{}.jpg"))
+# save_pic("https://www.artsy.net/artwork/amoako-boafo-untitled-2")
