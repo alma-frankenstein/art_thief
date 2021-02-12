96fa444692ce87e7e9a1d5e6a96c6d65047eb947
Alma Frankenstein <a.q.frankenstein@gmail.com>
2021-02-12 11:29:26 -0800

    commit changes before rebasing

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+""" Find the dimensions of an image. Get each tile and paste it onto a blank canvas, then
+    save that image by the image name and artist"""
+
 from io import BytesIO
-import requests
-from PIL import Image
-import logging
 from pathlib import Path
 import re
 from typing import Tuple
+from PIL import Image
+import requests
 from loggers import get_tiles_logger
 
 
 TILE_MAX_RANGE = 10
 TILE_SIZE = 512
 
-# TODO It should be safe to assume the first tile is enough to determine the maximum TILE_SIZE.
-#   If the image creation could be delayed until after the first tile is fetched we wouldn't need TILE_SIZE
+
 new_image = Image.new(
     'RGB', (TILE_SIZE * TILE_MAX_RANGE, TILE_SIZE * TILE_MAX_RANGE))
 
 
 def get_tiles_and_save(dz_url, title_artist):
+    """ Call other module functions to get all tiles for max dimensions,
+        crop off blank margin space, and save to tile tree"""
     width_counter, actual_width = find_max_width(dz_url)
     height_counter, actual_height = find_max_height(dz_url)
     get_tiles(dz_url, width_counter, height_counter)
@@ -29,18 +32,22 @@ def get_tiles_and_save(dz_url, title_artist):
         return False
     return True
 
+
 def paste_on_canvas(i, j, image_data):
+    """ Put each fetched tile in its proper place on the blank new_image canvas """
     im = Image.open(BytesIO(image_data))
     new_image.paste(im, (TILE_SIZE * i, TILE_SIZE * j))
     # print(f"Fetching image {i}_{j}.jpg")
-    
-    
+
+
 def remove_special(stem):
+    """ Remove special characters so the artwork title can be used as a file name """
     stem = re.sub(r"[^a-zA-Z0-9]+", ' ', stem)
     return stem
 
 
 def crop_and_save(actual_w, actual_h, title_artist):
+    """ Trim blank margins from the image canvas, save the image by piece name and artist """
     cropped_image = new_image.crop((0, 0, actual_w, actual_h))
     title_artist = remove_special(title_artist)
     p = Path.cwd().joinpath("saved_images", f"{title_artist}.jpg")
@@ -48,14 +55,17 @@ def crop_and_save(actual_w, actual_h, title_artist):
 
 
 def find_max_width(dz_url) -> Tuple[int, int]:
+    """ Return image width """
     return _find_max_dimension(dz_url, TILE_MAX_RANGE, find_width=True)
 
 
 def find_max_height(dz_url) -> Tuple[int, int]:
+    """ Return image height """
     return _find_max_dimension(dz_url, TILE_MAX_RANGE, find_width=False)
 
 
 def _find_max_dimension(dz_url, max_range, find_width: bool = True) -> Tuple[int, int]:
+    """ Traverse top row and left column to find image dimensions """
     root_url = dz_url
     actual_size = 0
     for dim in range(max_range):
@@ -87,6 +97,4 @@ def get_tiles(dz_url, w_counter, h_counter):
             paste_on_canvas(i, j, image_data=r.content)
 
 
-
 # # TODO Turn the whole thing into a flask app and host it on GH?
-
