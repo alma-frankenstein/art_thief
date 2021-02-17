48054f7164a4cc9e9ee26f009ff88dfd3b4889e1
Ryan Jarvis <ryan@technoloft.com>
2021-02-17 12:16:50 -0800

    Refactor and add a Route with Picture titles

--- src/app/pil_image.py
+++ src/app/pil_image.py
@@ -1,9 +1,10 @@
 from io import BytesIO
 
+from PIL.Image import Image
 from flask import send_file
 
 
-def serve_pil_image(pil_img):
+def serve_pil_image(pil_img: Image):
     img_io = BytesIO()
     pil_img.save(img_io, 'JPEG', quality=70)
     img_io.seek(0)
